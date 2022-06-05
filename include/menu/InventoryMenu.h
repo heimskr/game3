8d383f8603d7788aa4ac37b60f03eb5bd91960f9
Kai Tamkun <ktamkun@ucsc.edu>
2022-06-05 13:02:23 -0700

    Maybe use nanogui instead lmao

--- include/menu/InventoryMenu.h
+++ include/menu/InventoryMenu.h
@@ -2,16 +2,24 @@
 
 #include <memory>
 
+#include <nanogui/nanogui.h>
+
 #include "menu/Menu.h"
 
 namespace Game3 {
 	class Player;
 
-	struct InventoryMenu: Menu {
-		std::shared_ptr<Player> player;
-		InventoryMenu(const std::shared_ptr<Player> &player_): player(player_) {}
-		~InventoryMenu() override = default;
-		virtual void render(Game &, Canvas &, NVGcontext *) override;
-		MenuType getType() const override { return MenuType::Inventory; }
+	class InventoryMenu: public Menu {
+		public:
+			std::shared_ptr<Player> player;
+
+			InventoryMenu(const std::shared_ptr<Player> &player_): player(player_) {}
+			~InventoryMenu() override;
+
+			virtual void render(Game &, Canvas &, NVGcontext *) override;
+			MenuType getType() const override { return MenuType::Inventory; }
+
+		private:
+			nanogui::Window *window = nullptr;
 	};
 }
