1a13b3edfd2f0a2b9702e7495d158c082b7ddc91
Kai Tamkun <ktamkun@ucsc.edu>
2022-06-06 14:50:28 -0700

    JSONification for Position

--- src/Position.cpp
+++ src/Position.cpp
@@ -1,11 +1,13 @@
 #include "Position.h"
 
 namespace Game3 {
-	void to_json(nlohmann::json &, const Position &) {
-
+	void to_json(nlohmann::json &json, const Position &position) {
+		json[0] = position.row;
+		json[1] = position.column;
 	}
 
-	void from_json(const nlohmann::json &, Position &) {
-
+	void from_json(const nlohmann::json &json, Position &position) {
+		position.row = json.at(0);
+		position.column = json.at(1);
 	}
 }
