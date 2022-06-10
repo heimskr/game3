a369a2233c8a6172b173e29f13207dc683367913
Kai Tamkun <ktamkun@ucsc.edu>
2022-06-09 21:43:54 -0700

    Remove nanovg.cpp

--- src/lib/nanovg.cpp
+++
@@ -1,8 +0,0 @@
-extern "C" {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
-#pragma GCC diagnostic ignored "-Wmisleading-indentation"
-#pragma GCC diagnostic ignored "-Wshift-negative-value"
-#include "nanovg.c"
-#pragma GCC diagnostic pop
-}
