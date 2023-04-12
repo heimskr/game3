ca9fcdff7515a90ed1c052fd3e3f406085bb3da1
Kai Tamkun <ktamkun@ucsc.edu>
2023-04-11 20:32:22 -0700

    Fix clay landfills

--- src/item/Landfills.cpp
+++ src/item/Landfills.cpp
@@ -8,13 +8,13 @@ namespace Game3 {
 		const auto &tilename = place.getLayer1Name();
 
 		if (tilename == "base:tile/water"_id)
-			return Landfill::Result{ItemStack(game, "base:clay"_id, Landfill::DEFAULT_COUNT), "base:tile/shallow_water"_id};
+			return Landfill::Result{ItemStack(game, "base:item/clay"_id, Landfill::DEFAULT_COUNT), "base:tile/shallow_water"_id};
 
 		if (tilename == "base:tile/deep_water"_id)
-			return Landfill::Result{ItemStack(game, "base:clay"_id, Landfill::DEFAULT_COUNT), "base:tile/water"_id};
+			return Landfill::Result{ItemStack(game, "base:item/clay"_id, Landfill::DEFAULT_COUNT), "base:tile/water"_id};
 
 		if (tilename == "base:tile/deeper_water"_id)
-			return Landfill::Result{ItemStack(game, "base:clay"_id, Landfill::DEFAULT_COUNT), "base:tile/deep_water"_id};
+			return Landfill::Result{ItemStack(game, "base:item/clay"_id, Landfill::DEFAULT_COUNT), "base:tile/deep_water"_id};
 
 		return std::nullopt;
 	}
