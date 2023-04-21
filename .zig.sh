5713d9deed5966422b5b247eeea5d9849722445a
Kai Tamkun <ktamkun@ucsc.edu>
2023-04-21 02:13:41 -0700

    Actions: install zig

---
+++ .zig.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://ziglang.org/builds/zig-linux-x86_64-0.11.0-dev.2685+fac120bc3.tar.xz
+tar xf zig-linux-x86_64-0.11.0-dev.2685+fac120bc3.tar.xz
+sudo cp zig-linux-x86_64-0.11.0-dev.2685+fac120bc3/zig /usr/bin/zig
