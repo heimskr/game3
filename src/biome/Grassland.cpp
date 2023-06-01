d15fc077c4c152f14a0a310aa56a238913ab6ddc
Kai Tamkun <ktamkun@ucsc.edu>
2023-06-01 13:20:52 -0700

    Fix ghost trees

--- src/biome/Grassland.cpp
+++ src/biome/Grassland.cpp
@@ -90,8 +90,8 @@ namespace Game3 {
 		if (params.antiforestThreshold > perlin.GetValue(row / Biome::NOISE_ZOOM * factor, column / Biome::NOISE_ZOOM * factor, 0.)) {
 			if (auto tile = realm.tileEntityAt({row, column}); tile && tile->is("base:te/tree"_id) && !std::dynamic_pointer_cast<Tree>(tile)->hasHive()) {
 				Game &game = realm.getGame();
-				// realm.removeSafe(tile);
-				realm.queueRemoval(tile);
+				realm.removeSafe(tile);
+				// realm.queueRemoval(tile);
 				if (distribution(rng) < 3) {
 					std::vector<ItemStack> mushrooms {
 						{game, "base:item/saffron_milkcap"},
