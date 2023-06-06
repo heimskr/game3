19d07b05ce11d65b8aab37e32d2babdc35359acd
Kai Tamkun <ktamkun@ucsc.edu>
2023-06-06 05:35:09 -0700

    Nicer usage command output

--- src/command/local/UsageCommand.cpp
+++ src/command/local/UsageCommand.cpp
@@ -6,10 +6,9 @@
 
 namespace Game3 {
 	void UsageCommand::operator()(LocalClient &client) {
-		INFO("Bytes read: " << client.bytesRead);
-		INFO("Bytes written: " << client.bytesWritten);
+		std::cerr.imbue(std::locale(""));
 		INFO("Header bytes: " << client.headerBytes.size());
-		INFO("Payload size: " << client.payloadSize);
+		INFO("Payload size: \e[33m" << client.payloadSize << "\e[39m");
 		{
 			INFO("Packets received:");
 			std::shared_lock lock(client.receivedPacketCountsMutex);
@@ -22,5 +21,9 @@ namespace Game3 {
 			for (const auto &[packet_id, count]: client.sentPacketCounts)
 				INFO("    Packet " << packet_id << ": " << count);
 		}
+
+		INFO("Bytes read: \e[36m" << client.bytesRead << "\e[39m");
+		INFO("Bytes written: \e[35m" << client.bytesWritten << "\e[39m");
+		std::cerr.imbue(std::locale("C"));
 	}
 }
