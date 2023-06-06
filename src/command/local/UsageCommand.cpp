fdf2954b96ace0e06574da52fe4bc6d2ec4095e7
Kai Tamkun <ktamkun@ucsc.edu>
2023-06-06 05:39:50 -0700

    Even nicer usage command output

--- src/command/local/UsageCommand.cpp
+++ src/command/local/UsageCommand.cpp
@@ -7,19 +7,20 @@
 namespace Game3 {
 	void UsageCommand::operator()(LocalClient &client) {
 		std::cerr.imbue(std::locale(""));
-		INFO("Header bytes: " << client.headerBytes.size());
+		INFO("");
+		INFO("Header bytes: \e[3" << (client.headerBytes.empty()? '2' : '1') << 'm' << client.headerBytes.size() << "\e[39m");
 		INFO("Payload size: \e[33m" << client.payloadSize << "\e[39m");
 		{
 			INFO("Packets received:");
 			std::shared_lock lock(client.receivedPacketCountsMutex);
 			for (const auto &[packet_id, count]: client.receivedPacketCounts)
-				INFO("    Packet " << packet_id << ": " << count);
+				INFO("    " << std::setw(3) << std::right << packet_id << "\e[2m:\e[22m " << count);
 		}
 		{
 			INFO("Packets sent:");
 			std::shared_lock lock(client.sentPacketCountsMutex);
 			for (const auto &[packet_id, count]: client.sentPacketCounts)
-				INFO("    Packet " << packet_id << ": " << count);
+				INFO("    " << std::setw(3) << std::right << packet_id << "\e[2m:\e[22m " << count);
 		}
 
 		INFO("Bytes read: \e[36m" << client.bytesRead << "\e[39m");
