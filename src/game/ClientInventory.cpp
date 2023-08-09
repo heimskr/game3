dcee85203e11d204d3f51bdc3117ecaa2cc9a1b4
Kai Tamkun <ktamkun@ucsc.edu>
2023-08-08 23:48:05 -0700

    How'd that get in there

--- src/game/ClientInventory.cpp
+++ src/game/ClientInventory.cpp
@@ -131,7 +131,7 @@ namespace Game3 {
 		buffer += inventory.activeSlot.load();
 		{
 			auto &storage = inventory.getStorage();
-			auto lock = constorage.sharedLock();
+			auto lock = storage.sharedLock();
 			buffer += storage.getBase();
 		}
 		return buffer;
