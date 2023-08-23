629b5a35d3af6a8135228584790b0703e3a22039
Kai Tamkun <ktamkun@ucsc.edu>
2023-08-23 13:50:58 -0700

    Slightly more defensive .gtkmm4.sh

--- .gtkmm4.sh
+++ .gtkmm4.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 sudo pip3 install meson
 sudo pip3 install ninja
@@ -6,7 +6,10 @@ sudo pip3 install ninja
 mkdir .github-deps
 cd .github-deps
 
+ORIG_DIR=$(pwd)
+
 sudo apt remove libglib2.0-0 pkg-config
+sudo apt install gettext
 
 wget https://pkg-config.freedesktop.org/releases/pkg-config-0.29.2.tar.gz
 tar xf pkg-config-0.29.2.tar.gz
@@ -14,7 +17,7 @@ cd pkg-config-0.29.2
 ./configure --prefix=/usr --with-internal-glib
 make -j2
 sudo make install
-cd ..
+cd "$ORIG_DIR"
 export PKG_CONFIG_PATH="$(realpath prefix/lib/pkgconfig):/usr/lib/x86_64-linux-gnu/pkgconfig"
 
 wget https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/master/shared-mime-info-master.tar.gz
@@ -33,7 +36,7 @@ cd glib-2.77.0
 meson setup --prefix $(realpath ../prefix) --libdir lib -Dtests=false meson_build_prefix .
 cd meson_build_prefix
 ninja install
-cd ../..
+cd "$ORIG_DIR"
 
 wget https://github.com/silnrsi/graphite/archive/refs/heads/master.zip
 unzip master.zip
@@ -41,7 +44,7 @@ cd graphite-master
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr
 make -j2
 sudo make install
-cd ../..
+cd "$ORIG_DIR"
 
 wget https://download.gnome.org/sources/pango/1.51/pango-1.51.0.tar.xz
 tar xf pango-1.51.0.tar.xz
@@ -50,7 +53,7 @@ patch < ../../.pango.patch
 meson setup --prefix $(realpath ../prefix) --libdir lib meson_build .
 cd meson_build
 ninja install
-cd ../..
+cd "$ORIG_DIR"
 
 wget https://download.gnome.org/sources/gtk/4.12/gtk-4.12.0.tar.xz
 tar xf gtk-4.12.0.tar.xz
@@ -59,7 +62,7 @@ sed -i "s/graphene_dep,/graphene_dep,dependency('graphite2'),/" gtk/meson.build
 meson setup --prefix $(realpath ../prefix) --libdir lib -Dmedia-gstreamer=disabled -Dbuild-examples=false -Dbuild-tests=false -Dbuild-testsuite=false -Dbuild-demos=false -Dwayland:documentation=false -Dwayland:tests=false -Dlibsigcplusplus:build-documentation=false -Dlibsigcplusplus:build-examples=false -Dlibsigcplusplus:build-tests=false -Dlibsigcplusplus:validation=false -Dlibsigcplusplus:dist-warnings=no -Dlibsigcplusplus:warnings=no -Dsigc++-3.0:build-documentation=false -Dsigc++-3.0:build-examples=false -Dsigc++-3.0:build-tests=false -Dsigc++-3.0:validation=false -Dsigc++-3.0:dist-warnings=no -Dsigc++-3.0:warnings=no -Dwayland-protocols:tests=false -Dmm-common:use-network=true meson_build .
 cd meson_build
 ninja install
-cd ../..
+cd "$ORIG_DIR"
 
 wget https://download.gnome.org/sources/gtkmm/4.12/gtkmm-4.12.0.tar.xz
 tar xf gtkmm-4.12.0.tar.xz
