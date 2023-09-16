4a28a25996ddd8150a6d6ea594c89b34d1dac4a5
Kai Tamkun <ktamkun@ucsc.edu>
2023-09-16 02:31:31 -0700

    Don't immediately fail an assertion on oversized payloads

--- src/net/LocalClient.cpp
+++ src/net/LocalClient.cpp
@@ -59,8 +59,10 @@ namespace Game3 {
 					packetType  = headerBytes[0] | (static_cast<uint16_t>(headerBytes[1]) << 8);
 					payloadSize = headerBytes[2] | (static_cast<uint32_t>(headerBytes[3]) << 8) | (static_cast<uint32_t>(headerBytes[4]) << 16) | (static_cast<uint32_t>(headerBytes[5]) << 24);
 
-					if (100'000 <= payloadSize)
+					if (100'000 <= payloadSize) {
 						sock->close(false);
+						throw PacketError("Payload too large");
+					}
 
 					assert(payloadSize < 100'000);
 					headerBytes.erase(headerBytes.begin(), headerBytes.begin() + HEADER_SIZE);
