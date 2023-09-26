e07ef2e85cdddcb0be5e0474227cf5dcdf65fc71
Kai Tamkun <ktamkun@ucsc.edu>
2023-09-26 04:50:15 -0700

    Remove has_stack + printing from splitter

--- src/tools/Splitter.cpp
+++ src/tools/Splitter.cpp
@@ -37,8 +37,6 @@ namespace Game3 {
 			for (const auto &name: tileset.getTilesByCategory(category))
 				march[name] = corner;
 
-		std::set<Identifier> has_stack;
-
 		for (const auto &[name, id]: tileset.getIDs()) {
 			const int tileset_x = (id % 32) * 16;
 			const int tileset_y = (id / 32) * 16;
@@ -77,14 +75,8 @@ namespace Game3 {
 			if (auto stack_name_iter = tileset.stackNames.find(name); stack_name_iter != tileset.stackNames.end())
 				meta["stack"] = stack_name_iter->second;
 
-			if (meta.contains("stack"))
-				has_stack.insert(name);
-
 			std::ofstream of(dirname + "/tile.json");
 			of << meta.dump();
 		}
-
-		for (const Identifier &name: has_stack)
-			std::cout << name << '\n';
 	}
 }
