38c2af8562bda828702f054dceb0daf5741bfb9f
Kai Tamkun <ktamkun@ucsc.edu>
2023-09-26 12:21:28 -0700

    Fix carpet generation

--- src/worldgen/Carpet.cpp
+++ src/worldgen/Carpet.cpp
@@ -4,37 +4,38 @@
 
 namespace Game3::WorldGen {
 	void generateCarpet(const std::shared_ptr<Realm> &realm, std::default_random_engine &rng, Index width, Index height, int padding, Layer layer) {
-		const int carpet_offset = 8 * (rng() % 3);
-		const int carpet_padding = padding == -1? (rng() % 2) + 2 : padding;
+		const int carpet_padding = padding < 0? std::uniform_int_distribution(2, 3)(rng) : padding;
 		const auto &tileset = realm->getTileset();
 
-		const TileID carpet1c  = tileset["base:tile/carpet1_c"];
-		const TileID carpet1w  = tileset["base:tile/carpet1_w"];
-		const TileID carpet1e  = tileset["base:tile/carpet1_e"];
-		const TileID carpet1n  = tileset["base:tile/carpet1_n"];
-		const TileID carpet1s  = tileset["base:tile/carpet1_s"];
-		const TileID carpet1nw = tileset["base:tile/carpet1_nw"];
-		const TileID carpet1ne = tileset["base:tile/carpet1_ne"];
-		const TileID carpet1sw = tileset["base:tile/carpet1_sw"];
-		const TileID carpet1se = tileset["base:tile/carpet1_se"];
+		const std::string base = "base:tile/carpet" + std::to_string(std::uniform_int_distribution(1, 3)(rng)) + '_';
+
+		const TileID carpet1c  = tileset[Identifier(base +  'c')];
+		const TileID carpet1w  = tileset[Identifier(base +  'w')];
+		const TileID carpet1e  = tileset[Identifier(base +  'e')];
+		const TileID carpet1n  = tileset[Identifier(base +  'n')];
+		const TileID carpet1s  = tileset[Identifier(base +  's')];
+		const TileID carpet1nw = tileset[Identifier(base + "nw")];
+		const TileID carpet1ne = tileset[Identifier(base + "ne")];
+		const TileID carpet1sw = tileset[Identifier(base + "sw")];
+		const TileID carpet1se = tileset[Identifier(base + "se")];
 
 		for (int row = carpet_padding + 1; row < height - carpet_padding - 1; ++row)
 			for (int column = carpet_padding + 1; column < width - carpet_padding - 1; ++column)
-				realm->setTile(layer, {row, column}, carpet1c + carpet_offset, false, true);
+				realm->setTile(layer, {row, column}, carpet1c, false, true);
 
 		for (int row = carpet_padding + 1; row < height - carpet_padding - 1; ++row) {
-			realm->setTile(layer, {row, carpet_padding}, carpet1w + carpet_offset, false, true);
-			realm->setTile(layer, {row, width - carpet_padding - 1}, carpet1e + carpet_offset, false, true);
+			realm->setTile(layer, {row, carpet_padding}, carpet1w, false, true);
+			realm->setTile(layer, {row, width - carpet_padding - 1}, carpet1e, false, true);
 		}
 
 		for (int column = carpet_padding + 1; column < width - carpet_padding - 1; ++column) {
-			realm->setTile(layer, {carpet_padding, column}, carpet1n + carpet_offset, false, true);
-			realm->setTile(layer, {height - carpet_padding - 1, column}, carpet1s + carpet_offset, false, true);
+			realm->setTile(layer, {carpet_padding, column}, carpet1n, false, true);
+			realm->setTile(layer, {height - carpet_padding - 1, column}, carpet1s, false, true);
 		}
 
-		realm->setTile(layer, {carpet_padding, carpet_padding}, carpet1nw + carpet_offset, false, true);
-		realm->setTile(layer, {carpet_padding, width - carpet_padding - 1}, carpet1ne + carpet_offset, false, true);
-		realm->setTile(layer, {height - carpet_padding - 1, carpet_padding}, carpet1sw + carpet_offset, false, true);
-		realm->setTile(layer, {height - carpet_padding - 1, width - carpet_padding - 1}, carpet1se + carpet_offset, false, true);
+		realm->setTile(layer, {carpet_padding, carpet_padding}, carpet1nw, false, true);
+		realm->setTile(layer, {carpet_padding, width - carpet_padding - 1}, carpet1ne, false, true);
+		realm->setTile(layer, {height - carpet_padding - 1, carpet_padding}, carpet1sw, false, true);
+		realm->setTile(layer, {height - carpet_padding - 1, width - carpet_padding - 1}, carpet1se, false, true);
 	}
 }
