76f2d042ab6f01b09c5e4676a7cfe0d2a3222c88
Kai Tamkun <ktamkun@ucsc.edu>
2023-10-15 16:35:56 -0700

    Add lots more recipes (crafting + dissolver)

--- src/chemistry/MoleculeNames.cpp
+++ src/chemistry/MoleculeNames.cpp
@@ -37,5 +37,9 @@ namespace Game3 {
 		{"PbI2", "Lead iodide"},
 		{"NO3", "Nitrate"},
 		{"C40H56", "Beta carotene"},
+		{"C32H44O8", "Cucurbitacin E"},
+		{"C40H56", "Lycopene"},
+		{"C4H10", "Butane"},
+		{"C5H12", "Pentane"},
 	};
 }
