379d55b499d17609be8dc5f734575d16e671926d
Kai Tamkun <ktamkun@ucsc.edu>
2023-11-27 17:02:38 -0800

    Remove some old scripts

--- flatpak-cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -f /app/bin/libnoise*
-rm -f /app/bin/event_rpcgen.py
-rm -f /app/bin/mm-common*
-rm -rf /app/lib/debug
-echo "game3-cleanup complete :)"
--- hack-resources.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-OLD_DIR="$(pwd)"
-cd "$2"
-zig build-obj -O ReleaseSmall --main-pkg-path . src/resources.zig
-mv resources.o "$OLD_DIR/src/resources.o"
