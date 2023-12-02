569055275d93cafc8e9ff76dac1f6380a3022fdf
Kai Tamkun <ktamkun@ucsc.edu>
2023-12-02 12:03:54 -0800

    Proper histogram for skew test

--- src/test/DamageTest.cpp
+++ src/test/DamageTest.cpp
@@ -26,46 +26,12 @@ namespace Game3 {
 
 		const int factor = w.ws_col;
 
-		for (int i = 0; i < 10'000'000; ++i) {
+		for (int i = 0; i < 1'000'000; ++i) {
 			const auto quantile = boost::math::quantile(distribution, uniform(rng));
 			++samples[int(factor * quantile)];
 		}
 
-		size_t max_samples = 0;
-		for (const auto [n, count]: samples)
-			max_samples = std::max(max_samples, count);
-
-		const int digits = std::floor(std::log10(factor)) + 1;
-		const int height = w.ws_row - digits - 2;
-
-		const double line_scale = double(height) / max_samples;
-
-		auto move = [&](int x, int y) {
-			std::cout << "\e[" << (y + 1) << ';' << (x + 1) << 'H';
-		};
-
-		std::cout << "\e[2J\e[H";
-
-		for (int i = 0; i < factor; ++i) {
-			const size_t max = std::ceil(samples[i] * line_scale);
-
-			if (i == factor / 2)
-				std::cout << "\e[31m";
-
-			for (size_t j = 0; j < max; ++j) {
-				move(i, height - max + j);
-				std::cout << "█";
-			}
-
-			int digit = 0;
-			for (const char ch: std::to_string(i)) {
-				move(i, height + digit++);
-				std::cout << ch;
-			}
-
-			if (i == factor / 2)
-				std::cout << "\e[39m";
-		}
+		printHistogram(samples);
 	}
 
 	void damageTest(HitPoints weapon_damage, int defense, int variability, double attacker_luck, double defender_luck) {
