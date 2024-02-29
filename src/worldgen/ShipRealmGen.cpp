163147c66c8054f6a1b3ebb381fcfa624c3b8708
Kai Tamkun <ktamkun@ucsc.edu>
2024-02-29 00:06:03 -0800

    Update shipgen

--- src/worldgen/ShipRealmGen.cpp
+++ src/worldgen/ShipRealmGen.cpp
@@ -49,9 +49,17 @@ namespace Game3::WorldGen {
 			}
 
 			if (chunk_position == ChunkPosition{0, 0}) {
-				paster.ingest("empty;planks;tall_wooden_wall;sand;wooden_wall;bed2;grate_nw;grate_ne;ship_wheel;grate_mw;grate_me;barrier;grate_sw;grate_se/0,0=1:0:2,,3:0:2,,,,,1:0:2,1:0:4,,,,,,,,,,,,,/1,0=1:0:2,1:0:5,,,,,,1:0:2,1,,,,,,,,,,,,,1:0:4,/2,0=1:0:2,1,,,,,,1:0:2,1,,,,,,,,,1:6,1:7,1,,,1:0:4,/3,0=1:0:2,1,,,,,,,,,1:0:8,1,,,,,,1:9,1:10,1,,,,1:0:4,/4,0=1:0:2,1,,,,,,1:11,1,,,,,,,,,1:12,1:13,1,,,1:0:4,/5,0=1:0:2,1,,,,,,1:0:4,1,,,,,,,,,,,,,1:0:4,/6,0=1:0:4,,3:0:4,,,,,1:0:4,,,,,,,,,,,,,,");
+				paster.ingest("empty;planks;tall_wooden_wall;sand;wooden_wall;bed2;torch;barrel;rope;grate_nw;grate_ne;ship_wheel;grate_mw;grate_me;grate_sw;grate_se/0,0=1:0:2,3:0:2,,,,,1:0:2,1:0:4,,,,,,,,,,,,,/1,0=1:0:2,1:0:5,,,,,1:0:2,1:0:6,1,,,,,,,,1:0:7,1:8,1:0:7,1:0:6,1:0:4,/2,0=1:0:2,1,,,,,1:0:2,1,,,,,,,,,1:9,1:10,1,,,1:0:4,/3,0=1:0:2,1,,,,,,,,1:0:11,1,,,,,,1:12,1:13,1,,,,1:0:4,/4,0=1:0:2,1,,,,,1:0:4,1,,,,,,,,,1:14,1:15,1,,,1:0:4,/5,0=1:0:2,1,,,,,1:0:4,1:0:6,1:8,1,1:0:7,1,,,,,,1:0:7,1,1:0:6,1:0:4,/6,0=1:0:4,3:0:4,,,,,1:0:4,,,,,,,,,,,,,,");
 				paster.paste(*realm, chunk_position.topLeft() + Position{29, 23});
+			} else {
+				provider.updateChunk(chunk_position);
+				return;
 			}
+
+			chunk_position.iterate([&](const Position &position) {
+				for (Layer layer: allLayers)
+					realm->autotile(position, layer);
+			});
 
 			provider.updateChunk(chunk_position);
 		});
