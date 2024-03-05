0f0dd5d068eb92378bf95c2c0e3b5591fe217cfd
Kai Tamkun <ktamkun@ucsc.edu>
2024-03-05 03:45:42 -0800

    Add some resources

--- src/main.cpp
+++ src/main.cpp
@@ -163,7 +163,7 @@ int main(int argc, char **argv) {
 		}
 
 		if (argc == 4) {
-			std::cout << Game3::generateFlask(Game3::dataRoot / "resources" / "testtubebase.png", Game3::dataRoot / "resources" / "testtubemask.png", argv[1], argv[2], argv[3]);
+			std::cout << Game3::generateFlask(Game3::dataRoot / "resources" / "erlenmeyerbase.png", Game3::dataRoot / "resources" / "erlenmeyermask.png", argv[1], argv[2], argv[3]);
 			return 0;
 		}
 	}
