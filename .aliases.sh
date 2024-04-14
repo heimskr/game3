1774e0e880fd96c0daced3fb1a5a2c519e6559eb
Kai Tamkun <ktamkun@ucsc.edu>
2024-04-14 10:31:58 -0700

    Tidy .aliases.sh up a bit

--- .aliases.sh
+++ .aliases.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-b() { b_next() { ninja; cd ..; }; cd builddir && b_next; }
+b() { cd builddir && { ninja; cd .. } }
 alias reconf="meson setup --reconfigure builddir ."
 alias ts="./builddir/src/game3 -s"
 alias t="./builddir/src/game3"
 bts() { b && ts; }
 bt() { b && t; }
-ta() { ./builddir/src/game3 -s 12256 world.alt.db; }
-bta() { b && ta; }
