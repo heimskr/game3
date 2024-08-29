ce78ddf97d91c22338655c9c4cbe88aa345d56db
Kai Tamkun <ktamkun@ucsc.edu>
2024-08-28 20:28:49 -0700

    Lockable tabs

--- include/ui/gl/OmniDialog.h
+++ include/ui/gl/OmniDialog.h
@@ -17,7 +17,6 @@ namespace Game3 {
 		public:
 			std::shared_ptr<InventoryTab> inventoryTab;
 			std::shared_ptr<CraftingTab> craftingTab;
-			std::vector<std::shared_ptr<Tab>> tabs;
 			std::vector<Rectangle> tabRectangles;
 			std::shared_ptr<Tab> activeTab;
 
@@ -32,20 +31,25 @@ namespace Game3 {
 
 			template <typename T>
 			size_t removeTab() {
+				auto lock = tabs.uniqueLock();
 				return std::erase_if(tabs, &tabMatcher<T>);
 			}
 
 			template <typename T>
 			bool hasTab() const {
+				auto lock = tabs.sharedLock();
 				return std::ranges::any_of(tabs, &tabMatcher<T>);
 			}
 
 			template <typename T, typename... Args>
 			void addTab(Args &&...args) {
+				auto lock = tabs.uniqueLock();
 				tabs.emplace_back(std::make_shared<T>(ui, std::forward<Args>(args)...));
 			}
 
 		private:
+			Lockable<std::vector<std::shared_ptr<Tab>>> tabs;
+
 			template <typename T>
 			static bool tabMatcher(const std::shared_ptr<Tab> &tab) {
 				return dynamic_cast<const T *>(tab.get()) != nullptr;
