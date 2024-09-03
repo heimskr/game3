77022a9019932d951330335201108265f5d346c7
Kai Tamkun <ktamkun@ucsc.edu>
2024-09-03 13:09:33 -0700

    Empty the crafting tab

--- include/ui/gl/tab/CraftingTab.h
+++ include/ui/gl/tab/CraftingTab.h
@@ -5,13 +5,6 @@
 #include <memory>
 
 namespace Game3 {
-	class Box;
-	class Button;
-	class IconButton;
-	class Icon;
-	class ProgressBar;
-	class TextInput;
-
 	class CraftingTab: public Tab {
 		public:
 			using Tab::Tab;
@@ -19,13 +12,5 @@ namespace Game3 {
 			void init() final;
 			void render(UIContext &, const RendererContext &, float x, float y, float width, float height) final;
 			void renderIcon(const RendererContext &) final;
-
-		private:
-			std::shared_ptr<Box> box;
-			std::shared_ptr<ProgressBar> bar;
-			std::shared_ptr<TextInput> input;
-			std::shared_ptr<Button> button;
-			std::shared_ptr<IconButton> iconButton;
-			std::shared_ptr<Icon> icon;
 	};
 }
--- src/ui/gl/tab/CraftingTab.cpp
+++ src/ui/gl/tab/CraftingTab.cpp
@@ -2,12 +2,7 @@
 #include "graphics/Texture.h"
 #include "threading/ThreadContext.h"
 #include "ui/gl/tab/CraftingTab.h"
-#include "ui/gl/widget/Box.h"
-#include "ui/gl/widget/Button.h"
-#include "ui/gl/widget/IconButton.h"
-#include "ui/gl/widget/Icon.h"
-#include "ui/gl/widget/ProgressBar.h"
-#include "ui/gl/widget/TextInput.h"
+#include "ui/gl/widget/Scroller.h"
 #include "ui/gl/Constants.h"
 #include "ui/gl/UIContext.h"
 #include "util/Util.h"
@@ -16,108 +11,15 @@
 
 namespace Game3 {
 	void CraftingTab::init() {
-		assert(!bar);
-
 		auto tab = shared_from_this();
-
-		box = std::make_shared<Box>(scale);
-
-		input = std::make_shared<TextInput>(scale);
-		input->setFixedHeight(scale * TEXT_INPUT_HEIGHT_FACTOR);
-		input->setText(ui, "Hello from the crafting tab! This is some example text.");
-		input->onSubmit.connect([&](TextInput &input, UIContext &ui) {
-			Glib::ustring text = input.clear();
-			if (text.empty())
-				return;
-
-			float divisor = 1;
-			if (text.substr(text.length() - 1, 1) == "%") {
-				text.erase(text.length() - 1, 1);
-				divisor = 100;
-			}
-
-			float number{};
-			try {
-				number = parseNumber<float>(text.raw()) / divisor;
-			} catch (...) {
-				input.setText(ui, "Invalid number.");
-				return;
-			}
-
-			bar->setProgress(number);
-		});
-
-		bar = std::make_shared<ProgressBar>(scale, Color(1, 0, 0, 1), 0.5);
-		bar->setFixedHeight(10 * scale);
-		bar->setOnDragStart([this](Widget &, UIContext &ui, int, int) {
-			ui.addDragUpdater(bar);
-			return true;
-		});
-		bar->setOnDragUpdate([this](Widget &widget, UIContext &, int x, int) {
-			if (!widget.isDragging())
-				return false;
-
-			const Rectangle &last = widget.getLastRectangle();
-			const float scale = widget.getScale();
-
-			if (last.width > 2 * scale) {
-				x = std::min<int>(last.width - scale, std::max<int>(x, scale));
-				const float progress = static_cast<float>(x - scale) / static_cast<float>(last.width - 2 * scale);
-				bar->setProgress(progress);
-				button->setFixedHeight(scale * lerp(6.f, 32.f, progress));
-				input->setText(ui, std::format("{:.1f}%", progress * 100));
-			}
-
-			return true;
-		});
-
-		iconButton = std::make_shared<IconButton>(scale);
-		iconButton->setFixedHeight(scale * 12);
-		iconButton->setIconTexture(cacheTexture("resources/gui/randomize.png"));
-		iconButton->setOnClick([this](Widget &, UIContext &ui, int, int, int) {
-			const float height = threadContext.random(6.f, 32.f);
-			button->setFixedHeight(scale * height);
-			input->setText(ui, std::format("{} * {} = {}", scale, height, scale * height));
-			return true;
-		});
-
-		button = std::make_shared<Button>(scale);
-		button->setFixedHeight(scale * 10);
-		button->setText("Randomize");
-		button->setOnClick([&](Widget &, UIContext &, int, int, int) {
-			const float progress = threadContext.random(0.f, 1.f);
-			bar->setProgress(progress);
-			input->setText(ui, std::format("{:.1f}%", progress * 100));
-			return true;
-		});
-
-		icon = std::make_shared<Icon>(scale);
-		icon->setIconTexture(cacheTexture("resources/gui/settings.png"));
-		icon->setFixedSize(scale * 6);
-		icon->setOnClick([](Widget &, UIContext &, int, int, int) {
-			INFO("Icon clicked");
-			return true;
-		});
-
-		icon->insertAtEnd(tab);
-		box->insertAtEnd(tab);
-		bar->insertAtEnd(box);
-		input->insertAtEnd(box);
-		iconButton->insertAtEnd(box);
-		button->insertAtEnd(box);
+		auto scroller = std::make_shared<Scroller>(scale);
+		scroller->insertAtEnd(tab);
 	}
 
 	void CraftingTab::render(UIContext &ui, const RendererContext &renderers, float x, float y, float width, float height) {
-		box->render(ui, renderers, x, y, width, height);
-
-		const float icon_size = icon->getFixedHeight();
-		Rectangle rect = input->getLastRectangle() - ui.scissorStack.getTop().rectangle;
-		rect.x += rect.width - 2 * scale - icon_size;
-		rect.y += (rect.height - icon_size) / 2;
-		rect.width = icon_size;
-		rect.height = icon_size;
-
-		icon->render(ui, renderers, rect);
+		WidgetPtr child = firstChild;
+		assert(child != nullptr);
+		child->render(ui, renderers, x, y, width, height);
 	}
 
 	void CraftingTab::renderIcon(const RendererContext &renderers) {
