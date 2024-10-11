7484043c335259678bc538732c5b7d62e6cd0310
Kai Tamkun <ktamkun@ucsc.edu>
2024-10-11 12:21:04 -0700

    Clarify the purpose of the suspiciously named "discord-grabber.sh" script

--- src/discord-grabber.sh
+++ src/discord-grabber.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# I realize this filename sounds incredibly suspicious but this script is entirely harmless.
+# All it does is list all .cpp files in the `discord` directory at the root of the repo.
+# This is used during `meson setup`.
+
 find ../discord -name '*.cpp'
