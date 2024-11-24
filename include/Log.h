fae36908347fc6dd25040a4a5b26935705cc5206
Kai Tamkun <ktamkun@ucsc.edu>
2024-11-23 20:22:24 -0800

    no syncstream on macOS lol

--- include/Log.h
+++ include/Log.h
@@ -26,17 +26,25 @@ namespace Game3 {
 	template <typename... Args>
 	void SUCCESS(Args &&...) {}
 #else
+
+#ifdef __APPLE__
+#define DECLARE_STREAM auto &stream = std::cerr
+#else
+#define DECLARE_STREAM std::osyncstream stream{std::cerr}
+#endif
+
 #define LOG_START "\x1b[2m[\x1b[1m"
 #define LOG_INFO_MIDDLE "\x1b[22;2m]\x1b[22m (\x1b[22;1;34mi\x1b[22;39m)\x1b[2m ::\x1b[22m "
 #define LOG_WARN_MIDDLE "\x1b[22;2m]\x1b[22m (\x1b[22;1;33m!\x1b[22;39m)\x1b[2m ::\x1b[22m "
 #define LOG_ERROR_MIDDLE "\x1b[22;2m]\x1b[22m (\x1b[22;1;31m!\x1b[22;39m)\x1b[2m ::\x1b[22m "
 #define LOG_SPAM_MIDDLE "\x1b[22;2m]\x1b[22m (\x1b[22;1;35m_\x1b[22;39m)\x1b[2m :: "
 #define LOG_SUCCESS_MIDDLE "\x1b[22;2m]\x1b[22m (\x1b[22;1;32m🗸\x1b[22;39m)\x1b[2m :: \x1b[22;32m"
+
 	template <typename... Args>
 	void INFO(std::format_string<Args...> format, Args &&...args) {
-		std::osyncstream synced{std::cerr};
-		std::print(synced, "{}{}{}", LOG_START, Logger::getTimestamp(), LOG_INFO_MIDDLE);
-		std::println(synced, format, std::forward<Args>(args)...);
+		DECLARE_STREAM;
+		std::print(stream, "{}{}{}", LOG_START, Logger::getTimestamp(), LOG_INFO_MIDDLE);
+		std::println(stream, format, std::forward<Args>(args)...);
 	}
 
 	template <typename... Args>
@@ -48,9 +56,9 @@ namespace Game3 {
 
 	template <typename... Args>
 	void WARN(std::format_string<Args...> format, Args &&...args) {
-		std::osyncstream synced{std::cerr};
-		std::print(synced, "{}{}{}", LOG_START, Logger::getTimestamp(), LOG_WARN_MIDDLE);
-		std::println(synced, format, std::forward<Args>(args)...);
+		DECLARE_STREAM;
+		std::print(stream, "{}{}{}", LOG_START, Logger::getTimestamp(), LOG_WARN_MIDDLE);
+		std::println(stream, format, std::forward<Args>(args)...);
 	}
 
 	template <typename... Args>
@@ -62,9 +70,9 @@ namespace Game3 {
 
 	template <typename... Args>
 	void ERROR(std::format_string<Args...> format, Args &&...args) {
-		std::osyncstream synced{std::cerr};
-		std::print(synced, "{}{}{}", LOG_START, Logger::getTimestamp(), LOG_ERROR_MIDDLE);
-		std::println(synced, format, std::forward<Args>(args)...);
+		DECLARE_STREAM;
+		std::print(stream, "{}{}{}", LOG_START, Logger::getTimestamp(), LOG_ERROR_MIDDLE);
+		std::println(stream, format, std::forward<Args>(args)...);
 	}
 
 	template <typename... Args>
@@ -76,9 +84,9 @@ namespace Game3 {
 
 	template <typename... Args>
 	void SPAM(std::format_string<Args...> format, Args &&...args) {
-		std::osyncstream synced{std::cerr};
-		std::print(synced, "{}{}{}", LOG_START, Logger::getTimestamp(), LOG_SPAM_MIDDLE);
-		std::println(synced, format, std::forward<Args>(args)...);
+		DECLARE_STREAM;
+		std::print(stream, "{}{}{}", LOG_START, Logger::getTimestamp(), LOG_SPAM_MIDDLE);
+		std::println(stream, format, std::forward<Args>(args)...);
 	}
 
 	template <typename... Args>
@@ -90,10 +98,10 @@ namespace Game3 {
 
 	template <typename... Args>
 	void SUCCESS(std::format_string<Args...> format, Args &&...args) {
-		std::osyncstream synced{std::cerr};
-		std::print(synced, "{}{}{}", LOG_START, Logger::getTimestamp(), LOG_SUCCESS_MIDDLE);
-		std::print(synced, format, std::forward<Args>(args)...);
-		std::println(synced, "\x1b[39m");
+		DECLARE_STREAM;
+		std::print(stream, "{}{}{}", LOG_START, Logger::getTimestamp(), LOG_SUCCESS_MIDDLE);
+		std::print(stream, format, std::forward<Args>(args)...);
+		std::println(stream, "\x1b[39m");
 	}
 
 	template <typename... Args>
@@ -103,4 +111,5 @@ namespace Game3 {
 		}
 	}
 #endif
+#undef DECLARE_STREAM
 }
