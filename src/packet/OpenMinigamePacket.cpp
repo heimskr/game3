a826338a8f8132d73844199eec8c1f12f6475680
Kai Tamkun <ktamkun@ucsc.edu>
2025-03-30 03:43:16 -0700

    Don't crash when trying to open an unknown minigame

--- src/packet/OpenMinigamePacket.cpp
+++ src/packet/OpenMinigamePacket.cpp
@@ -6,12 +6,18 @@
 
 namespace Game3 {
 	OpenMinigamePacket::OpenMinigamePacket(Identifier minigame_id, int game_width, int game_height):
-		minigameID(std::move(minigame_id)), gameWidth(game_width), gameHeight(game_height) {}
+		minigameID(std::move(minigame_id)),
+		gameWidth(game_width),
+		gameHeight(game_height) {}
 
 	void OpenMinigamePacket::handle(const std::shared_ptr<ClientGame> &game) {
 		game->getWindow()->queue([game, id = minigameID, width = gameWidth, height = gameHeight](Window &window) {
 			auto &registry = game->registry<MinigameFactoryRegistry>();
-			window.uiContext.emplaceDialog<MinigameDialog>(1, (*registry.at(id))(game, {}), width, height);
+			if (auto factory = registry.maybe(id)) {
+				window.uiContext.emplaceDialog<MinigameDialog>(1, (*factory)(game, {}), width, height);
+			} else {
+				window.error(std::format("Unknown minigame: {}", id));
+			}
 		});
 	}
 }
