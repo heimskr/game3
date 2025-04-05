7cd496e63482c449795d442d4e2bb6f6f234a722
Kai Tamkun <ktamkun@ucsc.edu>
2025-04-04 23:49:31 -0700

    Add publish.sh

---
+++ publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TARGET="$1"
+
+if [ -z "$TARGET" ]; then
+	echo "Please specify a target."
+	exit 1
+fi
+
+if [ -z "$GAME3_REMOTE" ]; then
+	echo "Please set GAME3_REMOTE to username@host:path."
+	exit 2
+fi
+
+ZIP="game3-$TARGET-x86_64.zip"
+
+if [ ! -f "$ZIP" ]; then
+	echo "\"$ZIP\" doesn't exist."
+	exit 3
+fi
+
+scp "$ZIP" "$GAME3_REMOTE/$ZIP"
