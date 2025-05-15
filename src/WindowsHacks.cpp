c424b85cac62dcbace9fcaa25c5de2e523ab0dde
Kai Tamkun <ktamkun@ucsc.edu>
2025-05-15 02:43:12 -0700

    Add SHCreateMemStream hack

--- src/WindowsHacks.cpp
+++ src/WindowsHacks.cpp
@@ -5,8 +5,10 @@ extern "C" {
 	extern void _ZN9FastNoise22NewFromEncodedNodeTreeEPKcN8FastSIMD6eLevelE();
 	extern void _ZN9FastNoise16SmartNodeManager12IncReferenceEy();
 	extern void _ZN9FastNoise16SmartNodeManager12DecReferenceEyPvPFvS1_E();
+	extern void SHCreateMemStream();
 	const auto &__imp__ZN9FastNoise22NewFromEncodedNodeTreeEPKcN8FastSIMD6eLevelE = _ZN9FastNoise22NewFromEncodedNodeTreeEPKcN8FastSIMD6eLevelE;
 	const auto &__imp__ZN9FastNoise16SmartNodeManager12IncReferenceEy = _ZN9FastNoise16SmartNodeManager12IncReferenceEy;
 	const auto &__imp__ZN9FastNoise16SmartNodeManager12DecReferenceEyPvPFvS1_E = _ZN9FastNoise16SmartNodeManager12DecReferenceEyPvPFvS1_E;
+	const auto &__imp_SHCreateMemStream = SHCreateMemStream;
 }
 #endif
