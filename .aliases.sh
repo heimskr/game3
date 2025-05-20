ee2b8d9257b5e69ff6797232ec0e5561690c416b
Kai Tamkun <kai@tamkun.io>
2025-05-19 22:06:52 -0700

    Override CC in reconf alias

--- .aliases.sh
+++ .aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 b() { cd builddir && { ninja; cd ..; }; }
-alias reconf="CXX='ccache clang++' CXX_LD='mold' meson setup --reconfigure builddir ."
+alias reconf="CXX='ccache clang++' CC='ccache clang' CXX_LD='mold' meson setup --reconfigure builddir ."
 alias ts="./builddir/src/game3 -s"
 alias t="./builddir/src/game3"
 bts() { b && ts; }
