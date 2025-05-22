8645335bfd158765bde50aaf73e4601b24302f3f
Kai Tamkun <kai@tamkun.io>
2025-05-22 02:45:53 -0700

    Save file dialog is looking pretty good now

--- src/ui/gl/dialog/SaveFileDialog.cpp
+++ src/ui/gl/dialog/SaveFileDialog.cpp
@@ -1,4 +1,5 @@
 #include "ui/gl/dialog/SaveFileDialog.h"
+#include "ui/gl/widget/Icon.h"
 #include "ui/gl/widget/Scroller.h"
 #include "ui/gl/widget/TextInput.h"
 #include "ui/gl/UIContext.h"
@@ -9,14 +10,26 @@ namespace Game3 {
 
 		auto vbox = make<Box>(ui, selfScale, Orientation::Vertical, 0);
 
+		auto hbox = make<Box>(ui, selfScale, Orientation::Horizontal, 1, 0);
+
 		filenameInput = make<TextInput>(ui, selfScale);
+		filenameInput->setHorizontalExpand(true);
 		filenameInput->onSubmit.connect([this](TextInput &, const UString &text) {
 			submit(currentPath / text.raw());
 		});
 
+		auto ok_icon = make<Icon>(ui, selfScale, "resources/gui/yes.png");
+		ok_icon->setFixedSize(10);
+		ok_icon->setOnClick([this](Widget &) {
+			submit(currentPath / filenameInput->getText().raw());
+		});
+
+		hbox->append(filenameInput);
+		hbox->append(std::move(ok_icon));
+
 		remove(scroller);
 		vbox->append(scroller);
-		vbox->append(filenameInput);
+		vbox->append(std::move(hbox));
 		vbox->insertAtEnd(getSelf());
 	}
 
