078081210a657114e939c94f7f83d874e7f0085e
Kai Tamkun <kai@tamkun.io>
2025-06-06 09:31:57 +0200

    external_fastnoise2 option

--- .aliases.sh
+++ .aliases.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-b() { cd builddir && { ninja; cd ..; }; }
-alias reconf="CXX='ccache clang++' CC='ccache clang' CXX_LD='mold' meson setup --reconfigure builddir ."
-alias ts="./builddir/src/game3 -s"
-alias t="./builddir/src/game3"
+BUILDDIR="${GAME3_BUILDDIR:-builddir}"
+b() { cd $BUILDDIR && { ninja; cd ..; }; }
+alias reconf="CXX='ccache clang++' CC='ccache clang' CXX_LD='mold' meson setup --reconfigure $BUILDDIR ."
+alias ts="./$BUILDDIR/src/game3 -s"
+alias t="./$BUILDDIR/src/game3"
 bts() { b && ts; }
 bt() { b && t; }
 
