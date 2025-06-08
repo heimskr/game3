2bba2207c0ecc8f49098c0792d8fb7d6dd869f96
Kai Tamkun <kai@tamkun.io>
2025-06-08 14:13:29 -0700

    nanosleep64 hack that will probably not work

--- src/WindowsHacks.cpp
+++ src/WindowsHacks.cpp
@@ -6,9 +6,11 @@ extern "C" {
 	extern void _ZN9FastNoise16SmartNodeManager12IncReferenceEy();
 	extern void _ZN9FastNoise16SmartNodeManager12DecReferenceEyPvPFvS1_E();
 	extern void SHCreateMemStream();
+	extern void nanosleep();
 	const auto &__imp__ZN9FastNoise22NewFromEncodedNodeTreeEPKcN8FastSIMD6eLevelE = _ZN9FastNoise22NewFromEncodedNodeTreeEPKcN8FastSIMD6eLevelE;
 	const auto &__imp__ZN9FastNoise16SmartNodeManager12IncReferenceEy = _ZN9FastNoise16SmartNodeManager12IncReferenceEy;
 	const auto &__imp__ZN9FastNoise16SmartNodeManager12DecReferenceEyPvPFvS1_E = _ZN9FastNoise16SmartNodeManager12DecReferenceEyPvPFvS1_E;
 	const auto &__imp_SHCreateMemStream = SHCreateMemStream;
+	const auto &nanosleep64 = nanosleep;
 }
 #endif
