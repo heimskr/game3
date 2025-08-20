e7bec37e718b573975578c9dbc6f25d22ef7bd0c
Kai Tamkun <ktamkun@ucsc.edu>
2025-08-20 00:50:33 -0700

    Ignore drag action type in containment orbs

--- src/item/ContainmentOrb.cpp
+++ src/item/ContainmentOrb.cpp
@@ -38,12 +38,8 @@ namespace Game3 {
 		return true;
 	}
 
-	bool ContainmentOrb::drag(Slot slot, const ItemStackPtr &stack, const Place &place, Modifiers modifiers, std::pair<float, float> offsets, DragAction action) {
-		if (action == DragAction::Update) {
-			return use(slot, stack, place, modifiers, offsets);
-		}
-
-		return false;
+	bool ContainmentOrb::drag(Slot slot, const ItemStackPtr &stack, const Place &place, Modifiers modifiers, std::pair<float, float> offsets, DragAction) {
+		return use(slot, stack, place, modifiers, offsets);
 	}
 
 	std::string ContainmentOrb::getTooltip(const ConstItemStackPtr &stack) {
